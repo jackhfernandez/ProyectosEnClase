c9bb249350e4639927ffe4bfc63301d6a79bb1ef
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-10-26 00:58:26 -0500

    mejorando interfaz frmManMarca.java

--- appdae_04/src/capa_presentacion/frmMenu.java
+++ appdae_04/src/capa_presentacion/frmMenu.java
@@ -115,7 +115,7 @@ public class frmMenu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 636, Short.MAX_VALUE)
+            .addGap(0, 420, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
